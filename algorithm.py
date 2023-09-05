609ebf52019e3a3de67faa891eab725745d0c629
Fisher87 <992049896@qq.com>
2023-09-05 11:44:31 +0800

    update

--- algorithm.py
+++ algorithm.py
@@ -5048,3 +5048,62 @@ class MergetSort:
             nums[l : r+1] = tmp
         _sort(nums, 0, len(nums)-1)
         return nums
+
+class Rotate:
+    # 轮转数组 in-place
+    def rotate(self, nums, k):
+        def swap(nums, l, r):
+            while l<r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+
+        k %= len(nums)
+
+        swap(nums, 0, len(nums)-1)
+        swap(nums, 0, k-1)
+        swap(nums, k, len(nums)-1)
+
+class SearchMatrix:
+    def searchMatrix(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n-1
+        while (i>=0 and i<m) and (j>=0 and j<n):
+            if matrix[i][j] == target:
+                return True
+
+            if matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+
+        return False
+
+class permuteUnique:
+    def permuteUnique(self, nums):
+        self.ans = []
+
+        def dfs(nums, visited, path, depth, size):
+            if depth == size:
+                self.ans.append(path[:])
+                return
+
+            for i in range(size):
+                if visited[i]: 
+                    continue
+                if i>0 and nums[i]==nums[i-1] and visited[i-1] == False:
+                    continue
+                visited[i] = True
+                path.append(nums[i])
+                dfs(nums, visited, path, depth+1, size)
+                path.pop()
+                visited[i] = False
+
+        nums.sort()
+        size = len(nums)
+        visited = [ False ] * size
+        path = []
+        dfs(nums, visited, path, i, size)
+
+        return self.ans
+
