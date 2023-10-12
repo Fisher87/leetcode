bb263e44fa42095e30d4b43671b8c7ba8790af2f
Fisher87 <992049896@qq.com>
2023-10-12 11:20:27 +0800

    update

--- algorithm.py
+++ algorithm.py
@@ -6127,3 +6127,48 @@ class Solution:
             j = random.randrange(i, len(self.nums))
             self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
         return self.nums
+
+class CanFinish:
+    # 课程表问题 判断 DAG
+    def canFinish(self, numCourse, prerequisites):
+        indegrees = [0 for _ in range(numCourse)]
+        adjacency = [[] for _ in range(numCourse)]
+        queue = deque()
+
+        for cur, pre in prerequisites:
+            indegrees[cur] += 1
+            adjacency[pre].append(cur)
+
+        for i in range(len(indegrees)):
+            if not indegrees[i]:
+                queue.append(i)
+
+        while queue:
+            pre = queue.popleft()
+            numCourse -= 1
+            for cur in adjacency[pre]:
+                indegrees[cur] -= 1
+                if not indegrees[cur]: queue.append(cur)
+
+        return not numCourse
+
+    def canFinish(self, numCourses, prerequisites):
+        def dfs(i, adjacency, flags):
+            if flags[i] == -1:
+                return True
+            if flags[i] == 1:
+                return False
+
+            flags[i] = 1
+            for j in adjacency[i]:
+                if not dfs(j, adjacency, flags): return False
+            flags[i] = -1
+            return True
+
+        adjacency = [ [] for _ in range(numCourses) ]
+        flags = [0 for _ in range(numCourses)]
+        for cur, pre in prerequisites:
+            adjacency[pre].append(cur)
+        for i in range(numCourses):
+            if not dfs(i, adjacency, flags): return False
+        return True
